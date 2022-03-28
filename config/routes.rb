38a2b69c69f7737f6b22f2282cd4060be7535bb2
Ammar <mohammadammarkhan2@gmail.com>
2022-03-28 17:44:19 +0500

    Working on cart and cart item also add model and migration for coupon

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,15 @@ Rails.application.routes.draw do
     end
   end
 
-  get 'carts/index'
-  get 'cart_items/index'
+  resources :carts
+
+  post 'cart_items/:id/add' => "cart_items#add_quantity", as: "cart_item_add"
+  post 'cart_items/:id/reduce' => "cart_items#decrease_quantity", as: "cart_item_decrease"
+  post 'cart_items' => "cart_items#create"
+  get 'cart_items/new' => "cart_items#new"
+  get 'cart_items/:id' => "cart_items#show", as: "cart_item"
+  delete 'cart_items/:id' => "cart_items#destroy"
+
   devise_for :users
   get 'home/index'
   root to: "home#index"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_27_151043) do
+ActiveRecord::Schema.define(version: 2022_03_28_124115) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -52,6 +52,13 @@ ActiveRecord::Schema.define(version: 2022_03_27_151043) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+  end
+
+  create_table "coupons", force: :cascade do |t|
+    t.string "code"
+    t.float "discount"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "products", force: :cascade do |t|
