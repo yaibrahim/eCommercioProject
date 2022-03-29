35ca3d892f7b4152838633589041a0b907b50ce8
Ammar <mohammadammarkhan2@gmail.com>
2022-03-29 20:11:42 +0500

    Add validation in product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,6 +11,12 @@ class Product < ApplicationRecord
 
   delegate :full_name, to: :user
 
+  validates :name, :description, :price, presence: true
+  validates :description, length: { maximum: 30, too_long: '%{count} characters is the maximum allowed' }
+  validates :name, length: { minimum: 10, maximum: 50 }
+  validates :user_id, presence: true
+  validates :price, numericality: { only_integer: true }
+
   before_create :add_serial_number
 
   def add_serial_number
