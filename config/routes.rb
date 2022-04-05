89d98d60160df1fa51dc4cefb93143a17b7ce2fd
Hasan <hasanadeym@gmail.com>
2022-04-05 16:23:38 +0500

    misc review

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,27 @@
 Rails.application.routes.draw do
+  root to: 'products#index'
+
+  # i think some of the routes can be removed and functionality can be implemented in a more simpler way.
+
   resources :orders do
     collection do
-      get :detail
+      get :detail # for this, we could have made an order_items_controller having #index action. that's how we keep controllers THIN.
     end
   end
 
-  root to: 'products#index'
-
   resources :reviews do
     collection do
-      get :list
+      get :list # no need for this. we can manage this with #index
     end
   end
 
   resources :order_items
-  resources :carts
+  resources :carts # carts should be cart and this should be 'resource' route. not resources
+
   resources :cart_items, only: [:create, :new, :show, :destroy] do
     collection do
       post :add_quantity
-      post :decrease_quantity
+      post :decrease_quantity # decrease is more of a technical term. I think reduce_quantity is more appropriate
     end
   end
 
@@ -32,7 +35,7 @@ Rails.application.routes.draw do
   end
 
   resources :products do
-    resources :reviews
+    resources :reviews # i think there's no need to have #show
   end
 
   devise_for :users
