01c8a9f4b614935f22bc50744fab622c56fa04bb
Hasan <hasanadeym@gmail.com>
2022-04-05 16:23:23 +0500

    migrations review

--- db/migrate/20220331072444_create_orders.rb
+++ db/migrate/20220331072444_create_orders.rb
@@ -1,9 +1,9 @@
 class CreateOrders < ActiveRecord::Migration[6.1]
   def change
     create_table :orders do |t|
+      t.integer :coupon_id
       t.integer :user_id
       t.string :status
-      t.integer :coupon_id
 
       t.timestamps
     end
--- db/migrate/20220331094621_create_order_details.rb
+++ db/migrate/20220331094621_create_order_details.rb
@@ -1,10 +1,11 @@
 class CreateOrderDetails < ActiveRecord::Migration[6.1]
   def change
     create_table :order_details do |t|
+      t.integer :coupon_id
       t.integer :user_id
       t.string :status
-      t.integer :coupon_id
 
+      # seems duplicate of orders
       t.timestamps
     end
   end
