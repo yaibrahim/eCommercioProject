c09a3ed6899f9b2f027dac6f7e815b1b676917f8
Hasan <hasanadeym@gmail.com>
2022-04-05 16:22:48 +0500

    controllers review

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :update, :edit]
-  before_action :require_login, only: [:edit]
+  before_action :require_login, only: [:edit] # you should use devise provided callback authenticate_user etc
   before_action :require_user_edit, only: [:edit, :update]
 
   # include ProductsHelper
@@ -17,8 +17,7 @@ class ProductsController < ApplicationController
     @product = current_user.products.new(product_params)
 
     if @product.save
-      redirect_to product_path(@product)
-      flash[:notice] = 'Product added...'
+      redirect_to product_path(@product), notice: 'Product added...'
     else
       render :new
     end
@@ -39,14 +38,20 @@ class ProductsController < ApplicationController
   end
 
   def list
+    # @my_products = current_user.products
+    # @user_products = current_user.products
     @your_products = current_user.products
   end
 
   def search
-    if params[:search] == params[:search].capitalize
-      @search_result = Product.where("name LIKE ?","%" + params[:search].capitalize + "%")
+    # can we simplify this condition and use only one block?
+    # what if we do only the following? I think we can handle the capitilization in a different way
+      # Product.where('name LIKE ?', '%' + params[:search] + '%')
+
+    @search_result = if params[:search] == params[:search].capitalize
+      Product.where('name LIKE ?', '%' + params[:search].capitalize + '%')
     else
-      @search_result = Product.where("name LIKE ?","%" + params[:search] + "%")
+      Product.where('name LIKE ?', '%' + params[:search] + '%')
     end
   end
 
@@ -57,6 +62,7 @@ class ProductsController < ApplicationController
   end
 
   def set_product
+    # exception handling is missing in this project. Code will break incase end user sends a wrong id
     @product = Product.find(params[:id])
   end
 
@@ -64,16 +70,27 @@ class ProductsController < ApplicationController
     @product = current_user.products.find(params[:id])
   end
 
-  def require_user_edit
-    if @product.user_id != current_user.id
-      flash[:notice] = "You can't edit someones product"
-      redirect_to products_path
+  def require_user_edit # better method name would be def authorize_product_owner
+    if @product.user_id != current_user.id # if @product.not_an_owner? current_user.id
+      # flash[:notice] = "You can't edit someones product"
+      redirect_to products_path, notice: "You can't edit someones product" # one liner
     end
+
+    # owner methods in the product's model would look like the following
+    # def owner? current_user_id
+    #   self.user_id != current_user_id
+    # end
+
+    # def not_an_owner? current_user_id
+    #   !owner? current_user_id
+    # end
   end
 
-  def require_login
+  def require_login # authenticate_user is more appropriate method here.
+    # unless current_user?
     if !current_user.present?
-      flash[:notice] = "You can't edit someones product required"
+      flash[:notice] = "You can't edit someones product required" # this error message is incorrect. It should be 'You need to login to perform any action.'
+
       redirect_to products_path
     end
   end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-before_action :find_review, only: [:show, :edit, :update, :destroy]
+before_action :set_review, only: [:show, :edit, :update, :destroy]
 before_action :require_login, only: [:edit]
 before_action :require_user_edit, only: [:edit, :update]
 
@@ -11,7 +11,7 @@ before_action :require_user_edit, only: [:edit, :update]
   end
 
   def create
-    @review = current_user.reviews.new(set_review)
+    @review = current_user.reviews.new(review_params)
     if @review.save
       redirect_to product_review_path(@review.product_id, @review)
       flash[:notice] = 'review added...'
@@ -28,46 +28,46 @@ before_action :require_user_edit, only: [:edit, :update]
   end
 
   def update
-    respond_to do |format|
-      if @review.update(set_review)
-        format.html { redirect_to list_reviews_path, notice: 'Review was successfully updated.' }
-      else
-        format.html { render :list, status: 422 }
-      end
+    if @review.update(review_params)
+      redirect_to list_reviews_path, notice: 'Review was successfully updated.'
+    else
+      render :list, status: 422
     end
   end
 
   def list
-    @reviews = Review.all_reviews(current_user.id)
+    # this is better.
+    @reviews = current_user.reviews
+    # @reviews = Review.all_reviews(current_user.id)
   end
 
   def destroy
     puts request.format
+
     @review.destroy
 
-    respond_to do |format|
-      format.html { redirect_to products_url, notice: 'Reviews was successfully destroyed.' }
-    end
+    # what if review doesnt get destroyed for some reason?? we are not handling the errors here
+    redirect_to products_url, notice: 'Reviews was successfully destroyed.'
   end
 
   private
 
-  def set_review
+  def review_params
     params.require(:review).permit(:user_id, :product_id, :review_message)
   end
 
-  def find_review
+  def set_review
     @review = Review.find(params[:id])
   end
 
-  def require_user_edit
+  def require_user_edit # same as in products controller
     if @review.user_id != current_user.id
       flash[:alert] = "You can't edit or update others review.."
       redirect_to products_path
     end
   end
 
-  def require_login
+  def require_login # same as in products controller
     if !current_user.present?
       flash[:notice] = "You can't edit someones review required login"
       redirect_to products_path
