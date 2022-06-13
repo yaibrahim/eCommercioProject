ba6d0e049b334425502f53f63fcf96f6f94f889b
ibrahim <ibrahimazeem41@gmail.com>
2022-06-13 14:15:07 +0500

    Update in order controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,8 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
   def index
-    @orders = Order.where(user_id: current_user.id).paginate(page: params[:page], per_page: 4)
+    @orders = current_user.orders.paginate(page: params[:page], per_page: 4)
     authorize @orders
-
   end
 
   def new
