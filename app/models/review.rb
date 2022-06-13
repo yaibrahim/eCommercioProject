ca068fa10db3b3def14a05c0427deca1135616f8
ibrahim <ibrahimazeem41@gmail.com>
2022-06-13 14:10:31 +0500

    Update in review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,8 +4,6 @@ class Review < ApplicationRecord
 
   delegate :full_name, to: :user
 
-  scope :all_reviews, ->(user_id) { where(user_id: user_id) }
-
   delegate :name, to: :product, prefix: true
 
   def owner?(cuser_id)
