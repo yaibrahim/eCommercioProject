80ecf88963eb5c4ffbb8e53fd2d1fa2de6581b5a
ibrahim <ibrahimazeem41@gmail.com>
2022-06-14 19:14:23 +0500

    when user not login, and try to add to cart it will redirect to sign in page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :update, :edit]
-  before_action :authenticate_user!, except: [:search]
+  before_action :authenticate_user!, except: [:search, :show, :index]
 
   def index
     @products = Product.order(:id).paginate(page: params[:page], per_page: 15)
