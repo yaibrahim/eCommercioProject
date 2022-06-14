f99447d5e999787bc6a5b339aa73990733931a96
ibrahim <ibrahimazeem41@gmail.com>
2022-06-14 16:55:35 +0500

    Take first and last name and store full name in user record

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,12 @@ class User < ApplicationRecord
   has_many :products
   has_many :reviews
 
+  attr_accessor :first_name, :last_name
+
+  validates :full_name, presence: { message: 'can\'t be blank' }
+
+  before_validation :set_full_name
+
   has_one_attached :avatar
 
   after_create :assign_cart
@@ -13,5 +19,9 @@ class User < ApplicationRecord
 
   def assign_cart
     Cart.create(user_id: id)
+  end
+
+  def set_full_name
+    self.full_name = [first_name, last_name].join(' ')
   end
 end
