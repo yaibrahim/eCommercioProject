28684c07cbc928688bbcf577c6d82bde67f84123
ibrahim <ibrahimazeem41@gmail.com>
2022-06-15 18:18:42 +0500

    when user not signed in

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,6 +1,5 @@
 class CartItemsController < ApplicationController
   before_action :set_cart_item, only: [:edit, :destroy, :update]
-  #before_action :authenticate_user!, only: [:create]
 
   def new
     @cart_item = CartItem.new
@@ -8,9 +7,7 @@ class CartItemsController < ApplicationController
 
   def create
     if current_user.nil?
-      session[:cart] ||= []
-      session[:cart] << { 'quantity': cart_item_params[:quantity], 'product_id': cart_item_params[:product_id] }
-      redirect_to products_path, alert: session[:cart]
+      guest_cart
       return
     end
     @cart_item = CartItem.new(cart_item_params)
@@ -47,6 +44,18 @@ class CartItemsController < ApplicationController
   end
 
   private
+
+  def guest_cart
+    session[:cart] ||= []
+    item = { 'quantity': cart_item_params[:quantity], 'product_id': cart_item_params[:product_id] }
+    session[:cart].each_with_index do |cart_item, i|
+      if session[:cart][i]['product_id'] == item['product_id']
+        item[:quantity] = (item[:quantity].to_i + session[:cart][i]['quantity'].to_i).to_s
+      end
+    end
+    session[:cart] << item
+    redirect_to products_path, alert: session[:cart]
+  end
 
   def cart_item_params
     params.require(:cart_item).permit(:quantity, :product_id, :cart_id)
