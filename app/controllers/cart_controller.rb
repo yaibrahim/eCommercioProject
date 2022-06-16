b3323210267bd1c28eb039fc195804aa4abc97b8
ibrahim <ibrahimazeem41@gmail.com>
2022-06-16 13:34:46 +0500

    the items in the cart should remain intact, when user switch from guest login

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -2,7 +2,16 @@ class CartController < ApplicationController
   # before_action :authenticate_user!
   def index
     if current_user.present?
-      @cart = CartItem.user_cart(current_user.cart.id)
+      if session[:cart].present?
+        user_cart_items.delete_all
+        session[:cart].each_with_index do |cart_item, i|
+          add_guest_cart_items(session[:cart][i]['product_id'], session[:cart][i]['quantity'])
+        end
+        @cart = user_cart_items
+        session[:cart] = []
+      else
+        @cart = user_cart_items
+      end
     else
       @cart = session[:cart]
     end
@@ -17,5 +26,15 @@ class CartController < ApplicationController
       format.json { head :no_content }
       format.js
     end
+  end
+
+  private
+
+  def add_guest_cart_items(product_id, quantity)
+    CartItem.create('product_id': product_id, 'quantity': quantity, cart_id: current_user.cart.id)
+  end
+
+  def user_cart_items
+    CartItem.user_cart(current_user.cart.id)
   end
 end
--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -46,15 +46,17 @@ class CartItemsController < ApplicationController
   private
 
   def guest_cart
+    check = false
     session[:cart] ||= []
     item = { 'quantity': cart_item_params[:quantity], 'product_id': cart_item_params[:product_id] }
     session[:cart].each_with_index do |cart_item, i|
-      if session[:cart][i]['product_id'] == item['product_id']
-        item[:quantity] = (item[:quantity].to_i + session[:cart][i]['quantity'].to_i).to_s
+      if session[:cart][i]['product_id'] == item[:product_id]
+        session[:cart][i]['quantity'] = (item[:quantity].to_i + session[:cart][i]['quantity'].to_i).to_s
+        check = true
       end
     end
-    session[:cart] << item
-    redirect_to products_path, alert: session[:cart]
+    session[:cart] << item if check == false
+    redirect_to products_path, alert: 'Cart Item Added..'
   end
 
   def cart_item_params
